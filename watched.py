1ce36331a24c4a8c0e8d2e876e6d12130a5e2522
Bergrós Gígja <bergros.gigja@gmail.com>
2018-06-10 02:21:33 +0000

    Categorizes TV shows into their own folders if they don't exist. Fixed file ending issues.

--- watched.py
+++ watched.py
@@ -8,19 +8,23 @@ wtc = "C:\\Testing\\Watched" #Watched folder
 file = argv[1]
 
 end = 'AVI|MKV|MP4|M4V|FLV|WMV|MPG' #File endings
-video = re.match(r'(.*)(\.' + end + ')$', file) #Check if the file is a video
+video = re.match(r'(.*)\.(' + end + ')$', file) #Check if the file is a video
 
 if video:
 	name = video.group(1).replace('.', ' ').strip() #Remove dots between words
-
+	print(video.group(2))
 	#Match TV shows with name format S00E00
 	#Match Movies with 1080p in title
-	tv = re.match(r'((.*)([S|s][0-9]{1,2}[E|e][0-9]{1,2}))(.*)', name)
+	tv = re.match(r'(.*)([S|s][0-9]{1,2}[E|e][0-9]{1,2})(.*)', name)
 	movie = re.match(r'(.*1080p)(.*)', name)
 
 	#Strip away unnecessary title information
 	if tv:
-		name = tv.group(1).strip() + '.' + video.group(2).strip()
+		#Create folder for TV show if it doesn't exist
+		if not os.path.exists(wtc + "\\" + tv.group(1).strip()):
+			wtc = wtc + "\\" + tv.group(1).strip()
+			os.mkdir(wtc)
+		name = tv.group(1) + tv.group(2).strip() + '.' + video.group(2).strip()
 	if movie:
 		name = movie.group(1).strip() + '.' + video.group(2).strip()
 
