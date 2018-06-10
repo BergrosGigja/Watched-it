5ee11b4d5a6eadf09146420d0cf0dcc59743bb6d
Bergrós Gígja <bergros.gigja@gmail.com>
2018-06-10 01:24:07 +0000

    Only match video files. Beautify file names for files that have common name format.

--- watched.py
+++ watched.py
@@ -1,10 +1,28 @@
 from pathlib import Path
 import os
+import re
 from sys import argv
 
 dwl = "C:\\Testing\\Downloads" #Downloads folder
 wtc = "C:\\Testing\\Watched" #Watched folder
 file = argv[1]
 
-os.rename(dwl + "\\" +file, wtc + "\\" +file) #Move file
-os.startfile(wtc + "\\" +file) #Open file
+end = 'AVI|MKV|MP4|M4V|FLV|WMV|MPG' #File endings
+video = re.match(r'(.*)(\.' + end + ')$', file) #Check if the file is a video
+
+if video:
+	name = video.group(1).replace('.', ' ').strip() #Remove dots between words
+
+	#Match TV shows with name format S00E00
+	#Match Movies with 1080p in title
+	tv = re.match(r'((.*)([S|s][0-9]{1,2}[E|e][0-9]{1,2}))(.*)', name)
+	movie = re.match(r'(.*1080p)(.*)', name)
+
+	#Strip away unnecessary title information
+	if tv:
+		name = tv.group(1).strip() + '.' + video.group(2).strip()
+	if movie:
+		name = movie.group(1).strip() + '.' + video.group(2).strip()
+
+	os.rename(dwl + "\\" + file, wtc + "\\" + name) #Move file
+	os.startfile(wtc + "\\" + name) #Open file
